e8b3aa02622d6a9206dec1c7f4aad045fb9e2e64
jacobbenaim <jacobjbenaim@gmail.com>
2021-10-07 15:43:00 -0400

    fixed build

--- redux/reducers/rootReducer.ts
+++ redux/reducers/rootReducer.ts
@@ -1,14 +1,12 @@
 import { combineReducers } from "redux"
 import responsive from "./responsive"
 import errors from './errorReducers'
-import { firebaseReducer } from "react-redux-firebase";
 import { firestoreReducer } from "redux-firestore";
 
 const rootReducer = combineReducers({
   errors,
   responsive,
   firestore: firestoreReducer,
-  firebase: firebaseReducer,
 })
 
 export default rootReducer;
